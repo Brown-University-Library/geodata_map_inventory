4366b64e53f972068bd592e30cf540b4ea20537c
mciethan <emaclex@gmail.com>
2022-06-24 16:23:23 -0400

    started getting drop-downs to interact with each other based on selections

--- display.py
+++ display.py
@@ -1,27 +1,58 @@
 # Ethan McIntosh - GIS and Data Services - Brown University - summer 2022
 
-# This is where the GUI will actually run.  This is where the user view is 
-# configured, and where user selections are handled through calls to the backend
+# This is where the GUI will actually run (where the user window is configured, 
+# and where calls to the backend are made in response to user selections)
 
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-from tkinter.messagebox import showinfo  # for messageboxes, if desired
+# from tkinter.messagebox import showinfo  # for messageboxes, if desired
+import file_io
 
 tool_title = 'Brown University Map Collection Inventory Tool'
 
+# -------------- methods under construction -----------------
+def record_exception():
+    """
+    Defines action that's taken when the Record exception button is pressed
+    """
+    pass
+
+def remove_record():
+    """
+    Defines action that's taken when the Remove selected record button is pressed
+    """
+    pass
+
+def we_have_1():
+    """
+    Defines action that's taken when the We have 1 button is pressed
+    """
+    pass
+
+def we_have_multiple():
+    """
+    Defines action that's taken when the We have multiple button is pressed
+    """
+    pass
+
+# ------------------- initialize tkinter window -----------------
+
 root = tk.Tk()
 root.title(tool_title)
-# root.geometry('820x250') # to manually set the size
+# root.geometry('1220x250') # to manually set the size - shouldn't be necessary
 
+# Frame containing the entire window - exists in order to customize margins
 content = tk.Frame(root)
 content.grid(row=0, column=0, padx=10, pady=20)
 
+# ---------------------- header frame ----------------------------
 header = tk.Frame(content)
 header.grid(row=0, column=0, columnspan=5, rowspan=1, pady=5)
 title = tk.Label(header, text=tool_title)
 title.grid(row=0, column=0, columnspan=5, rowspan=1, pady=5)
 
+# -------------------- options frame ----------------------
 options = tk.Frame(content)
 options.grid(row=1, column=0, columnspan=5, rowspan=5, pady=5)
 dropdowns = {}
@@ -30,40 +61,48 @@ class LabeledDropDownMenu:
         self.label = label
         self.menu = menu
 label_names = ['Map Scale', 'Primary State', 'Cell Name', 'Map Year', 'Print Year']
-for lbl in label_names:
+for idx, lbl in enumerate(label_names):
     dropdowns[lbl] = LabeledDropDownMenu(ttk.Label(options, text=lbl + ": ")
         , ttk.Combobox(options, textvariable=StringVar(), state=DISABLED))
 # for example, dropdowns['Cell Name'].menu['state'] = 'readonly'
 
+# gridding the drop downs onto the frame
 for idx, dd in enumerate(dropdowns.values()):
     r = idx%3 + 1  # creates three rows of drop downs, starting at row 1, with
     c = int(idx/3)*2 # as many columns as are needed for the set of drop downs
-    side = tk.W if idx < 3 else tk.E
     dd.label.grid(row=r, column=c, padx=20, pady=10)
     dd.menu.grid(row=r, column=c+1, padx=20)
+
+# set up possible values for drop-downs
+maps = {}
+file_io.read_topos('usgs_topos.csv', maps)
+dd = dropdowns['Map Scale'].menu
+dd['values'] = sorted(list(maps.keys()))
+dd['state'] = 'readonly'
+
+def scaleSelected(event): # control other dropdowns in response to selections
+    dropdowns['Primary State'].menu['values'] = sorted(list(maps[int(dd.get())].keys()))
+    dropdowns['Primary State'].menu.set('')
+    dropdowns['Primary State'].menu['state'] = 'readonly'
+dd.bind('<<ComboboxSelected>>', scaleSelected) 
+
+# need to think through how button click methods are organized. i'd love to have the
+# dropdowns in an ordered list, and then have methods that activate the "next"
+# menus, or if making a selection of something earlier, deactivate previous ones
+# Except at any given time, someone could be choosing any of the buttons. so i 
+# do need named references to each one, i think.  Could still order them somehow
+# though.  
 
 dmgvar = BooleanVar(value=False)
 damaged = ttk.Checkbutton(options, text="Something is significantly damaged", 
             variable=dmgvar, onvalue=True)
 damaged.grid(row=3, column=2, columnspan=2)
 
-def record_exception():
-    pass
-
-def remove_record():
-    pass
-
-def we_have_1():
-    pass
-
-def we_have_multiple():
-    pass
-
 exception_btn = ttk.Button(options, text='Record an exception', command=record_exception)
-exception_btn.grid(row=4, column=0, columnspan=2, pady=5)
+exception_btn.grid(row=4, column=0, pady=5)
 
 remove_btn = ttk.Button(options, text='Remove selected record', command=remove_record)
-remove_btn.grid(row=5, column=0, columnspan=2, pady=5)
+remove_btn.grid(row=5, column=0, pady=5)
 
 add1_btn = ttk.Button(options, text='We have 1', command=we_have_1)
 add1_btn.grid(row=4, column=2, rowspan=2, pady=5)
@@ -71,6 +110,7 @@ add1_btn.grid(row=4, column=2, rowspan=2, pady=5)
 add_mult_btn = ttk.Button(options, text='We have multiple', command=we_have_multiple)
 add_mult_btn.grid(row=4, column=3, rowspan=2, pady=5)
 
+# ---------------- table frame ---------------------------
 table = tk.Frame(content)
 table.grid(row=6, column=0, columnspan=5, rowspan=1, pady=5)
 tbl_cols = ('Producer', 'Map Scale', 'Primary State', 'Cell Name', 'Map Year', 'Print Year')
